c38826cb4993bf75234dc5a04fcee4acde8f16a9
Mário Fernando Gomes Neto <mariosoares20012005@gmail.com>
2026-09-12 12:15:42 +0100

    feat: Type anotations and type inference

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-
 // Começsndo com typescript
 
+// Type anotations -> anotação de tipos
+const myName: string = "Mário";
 
-const myName : string = "Mário"
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(12, 12);
+
+function greet(name: string, age?: number, isProgrammer?: boolean) {
+  console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+greet("Mário", 30);
